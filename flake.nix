a87043c8257d6c3de451e0c1c9dac33b0677f6f1
Özgür Isbert <oezguerisbert@gmail.com>
2023-07-13 16:32:02 +0200

    my setup, WIP (first try?)

--- flake.nix
+++ flake.nix
@@ -2,8 +2,6 @@
   description = "Development packages and systems for oezguerisbert. Inspired by 'blackglasses' at TheAltF4Stream";
 
   inputs = {
-    darwin.inputs.nixpkgs.follows = "nixpkgs";
-    darwin.url = "github:lnl7/nix-darwin";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -14,7 +12,7 @@
       systems = import ./system { inherit inputs; };
     in
     flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "aarch64-darwin" "aarch64-linux" "x86_64-darwin" "x86_64-linux" ];
+      systems = [ "x86_64-linux" ];
       perSystem = { config, self', inputs', pkgs, system, ... }: {
         packages = {
           oezguerisbert-nvim = pkgs.vimUtils.buildVimPlugin {
@@ -25,19 +23,6 @@
       };
 
       flake = {
-        darwinConfigurations = {
-          oezguerisbert-darwin = systems.mkDarwin {
-            system = "x86_64-darwin";
-            username = "oezguerisbert";
-          };
-
-          work-darwin = systems.mkDarwin {
-            system = "aarch64-darwin";
-            username = "oisbert";
-          };
-        };
-
-
         nixosConfigurations = {
           oezguerisbert-nixos = systems.mkNixOS {
             desktop = true;
